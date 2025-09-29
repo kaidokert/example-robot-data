82d4ed1d85f0fbe88879df1928ba1ba06fd635d6
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-09-29 20:04:44 +0200

    release: Update CHANGELOG.md for 4.4.0

--- python/example_robot_data/robots_loader.py
+++ python/example_robot_data/robots_loader.py
@@ -425,6 +425,15 @@ class IrisLoader(RobotLoader):
     urdf_filename = "iris_simple.urdf"
     free_flyer = True
 
+class SO100Loader(RobotLoader):
+    path = "so_arm_description"
+    urdf_filename = "so100.urdf"
+    urdf_subpath = "urdf"
+
+class SO101Loader(RobotLoader):
+    path = "so_arm_description"
+    urdf_filename = "so101.urdf"
+    urdf_subpath = "urdf"
 
 class PR2Loader(RobotLoader):
     path = "pr2_description"
@@ -511,6 +520,8 @@ ROBOTS = {
     "ur10": UR10Loader,
     "ur10_limited": UR10LimitedLoader,
     "xarm7": xArm7Loader,
+    "so100": SO100Loader,
+    "so101": SO101Loader,
 }
 
 
